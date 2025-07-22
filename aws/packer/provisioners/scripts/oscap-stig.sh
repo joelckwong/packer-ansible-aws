a6ec0e9a95c457b8dffba87c5c29c138ef496cd6
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2025-07-22 05:41:51 -0400

    Update oscap-stig.sh

--- aws/packer/provisioners/scripts/oscap-stig.sh
+++ aws/packer/provisioners/scripts/oscap-stig.sh
@@ -2,6 +2,8 @@
 set -ex
 # Run openscap stig scan
 sudo yum install -y openscap-scanner scap-security-guide audispd-plugins libcap-ng-utils unzip
+sudo sed -i 's/nopasswd.*true/d' /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml
+sudo oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig --results ~/oscap-results.xml --report ~/oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml
 sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig --results ~/oscap-results.xml --report ~/oscap-report.html --fetch-remote-resources /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml
 curl -O https://public.cyber.mil/wp-content/uploads/stigs/zip/scc-5.10.2_rhel8_oracle-linux8_x86_64_bundle.zip
 unzip scc-5.10.2_rhel8_oracle-linux8_x86_64_bundle.zip
